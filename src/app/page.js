5d907a630694d7d0fd4ad7991189da4ed4ccd4bd
Ujjwal Katiyar <ukcode07@gmail.com>
2025-04-10 22:34:12 +0530

    add EmberParticles component for dynamic particle effects

--- src/app/page.js
+++ src/app/page.js
@@ -18,36 +18,38 @@ import Coordinator from './components/Coordinator';
 import Communit from './components/Communit';
 import Chief from './components/Chief';
 import Supported from './components/Supported';
-
+import { EmberParticles } from './components/EmberParticles';
 
 export default function Home() {
   const [isClient, setIsClient] = useState(false)
- 
+
   useEffect(() => {
     setIsClient(true)
   }, [])
   return (
     <>
       <div className="relative bg-[rgb(236,236,221)] z-0 before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-[url('/bg2.jpg')] before:bg-no-repeat before:bg-center before:-z-10 before:bg-cover before:bg-fixed before:brightness-[30%] ">
-        
-      {/* <h1>{isClient ? 'This is never prerendered' : 'Prerende red'}</h1> */}
-      <Header />
+        <div className="fixed inset-0 z-0">
+          <EmberParticles containerId="ember-container" />
+        </div>
+        {/* <h1>{isClient ? 'This is never prerendered' : 'Prerende red'}</h1> */}
+        <Header />
         <About />
         <Track />
         <Rules />
         {/* <Versions/> */}
-        <Prizes/>
+        <Prizes />
         <Sponsor />
-        <Chief/>
-        <Pateron /> 
-        <Convenor/> 
-        <Coordinator/>
-        <Supported/>
-        <Team/>
-        <Communit/>
-        <Contact/>
-        <Footer/>
+        <Chief />
+        <Pateron />
+        <Convenor />
+        <Coordinator />
+        <Supported />
+        <Team />
+        <Communit />
+        <Contact />
+        <Footer />
       </div>
     </>
-  ); 
+  );
 }
